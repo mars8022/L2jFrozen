9b6c8f14013435a51ca29af6885de9dbf0498911
Shyla <Shyla@5215a6b0-ad85-4b3a-91a5-8dbf2feb3788>
2010-07-28 18:43:51 +0000

    Full Builds will not delete the destination folder to save the Compiled Revisions

--- trunk/Server/sql/updates/gm_shop_teon.sql
+++ trunk/Server/sql/updates/gm_shop_teon.sql
@@ -1,4 +1,3 @@
-﻿
 delete from `custom_npc` where id = 53;
 INSERT INTO `custom_npc` VALUES
 (53, 20830, 'GM Shop', 1, 'L2Frozen', 1, 'Monster.angel', 13.50, 36.50, 72, 'male', 'L2Merchant', 40, 4013, 1565, 53.72, 3.09, 40, 43, 30, 21, 20, 10, 0, 0, 1710, 406, 849, 399, 278, 500, 333, 148, 0, 0, 33, 187, 'tower_guard_clan', 400, 0, 10, 'LAST_HIT', 0, 0, 0, 'fighter');
