54a4d0dd4430aada426434237b4e099dfe9bbbee
beetle <beetle@5215a6b0-ad85-4b3a-91a5-8dbf2feb3788>
2010-08-20 08:59:23 +0000

    Fixed RegisterGameServer bat file

--- gameserver/dist/RegisterGameServer.bat
+++ gameserver/dist/RegisterGameServer.bat
@@ -1,2 +1,2 @@
-@java -Djava.util.logging.config.file=console.cfg -cp lib/*:lib/l2jfrozen-core.jar com.l2jfrozen.gsregistering.GameServerRegister
+@java -Djava.util.logging.config.file=console.cfg -cp lib/*;lib/l2jfrozen-core.jar com.l2jfrozen.gsregistering.GameServerRegister
 @pause
