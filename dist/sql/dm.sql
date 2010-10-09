597f551ebd39b65da328af5e62b92a48d835d0a8
Shyla <Shyla@5215a6b0-ad85-4b3a-91a5-8dbf2feb3788>
2010-10-09 12:39:56 +0000

    New Compiled Revision; no Current Time System.out

--- dist/sql/dm.sql
+++ dist/sql/dm.sql
@@ -13,10 +13,19 @@ CREATE TABLE `dm` (
   `npcX` int(11) NOT NULL default '0',
   `npcY` int(11) NOT NULL default '0',
   `npcZ` int(11) NOT NULL default '0',
+  		`npcHeading` int(11) NOT NULL DEFAULT '0',
   `rewardId` int(11) NOT NULL default '0',
   `rewardAmount` int(11) NOT NULL default '0',
+  		`joinTime` int(11) NOT NULL DEFAULT '0',
+  		`eventTime` int(11) NOT NULL DEFAULT '0',
+  		`minPlayers` int(11) NOT NULL DEFAULT '0',
+  		`maxPlayers` int(11) NOT NULL DEFAULT '0',
   `color` int(11) NOT NULL default '0',
   `playerX` int(11) NOT NULL default '0',
   `playerY` int(11) NOT NULL default '0',
-  `playerZ` int(11) NOT NULL default '0'  
+  `playerZ` int(11) NOT NULL default '0',
+  		`delayForNextEvent` BIGINT NOT NULL DEFAULT '0'
 ) DEFAULT CHARSET=utf8;
+
+INSERT INTO `dm` values 
+('DM', 'A PvP Event', 'Giran', 1, 81, 70014, 82580, 148552, -3468, 1, 8752, 1, 2, 5, 2, 50, 2552550, 116615, 76200, -2729, 300000);
