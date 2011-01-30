025b92fab5872792623f59ec85b0ab11463089b7
Shyla <Shyla@5215a6b0-ad85-4b3a-91a5-8dbf2feb3788>
2011-01-30 00:56:32 +0000

    Missing Van Halter stats

---
+++ dist/sql/updates/update_494.sql
@@ -0,0 +1,2 @@
+delete from npc where id = 29062;
+INSERT INTO `npc` VALUES (29062,29062,'High Priestess van Halter',0,'Raid Boss',0,'Monster3.heretic_priest_110p','9.00','21.30',80,'male','L2RaidBoss',40,'115708','1866',  '364',  '3.21'   ,60   ,57   ,73  ,76   ,70    ,80   ,2915973,2380617,'5253',   '5343','2095',  '889'   ,230,  500   ,333, '0', '0', '0', '88', '132', null, '0', '0', '0', 'LAST_HIT');
