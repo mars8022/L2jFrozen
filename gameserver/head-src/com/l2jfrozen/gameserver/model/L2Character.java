8ac57808583d0ed64a77e21193d6ff2b706c8908
Nefer <nefer@5215a6b0-ad85-4b3a-91a5-8dbf2feb3788>
2011-02-11 21:47:48 +0000

    RB heal config fix.

--- gameserver/head-src/com/l2jfrozen/gameserver/model/L2Character.java
+++ gameserver/head-src/com/l2jfrozen/gameserver/model/L2Character.java
@@ -1587,8 +1587,8 @@ public abstract class L2Character extends L2Object
 		}
 
 		 // Player can't heal rb config
-		 if(!Config.PLAYERS_CAN_HEAL_RB && this!=null && !((L2PcInstance) this).isGM() && (target instanceof L2RaidBossInstance || target instanceof L2GrandBossInstance) && (skill.getSkillType() == SkillType.HEAL || skill.getSkillType() == SkillType.HEAL_PERCENT))
-		 {
+		if(!Config.PLAYERS_CAN_HEAL_RB && this instanceof L2PcInstance && !((L2PcInstance) this).isGM() && (target instanceof L2RaidBossInstance || target instanceof L2GrandBossInstance) && (skill.getSkillType() == SkillType.HEAL || skill.getSkillType() == SkillType.HEAL_PERCENT))
+		{
 		  this.sendPacket( ActionFailed.STATIC_PACKET );
 		  return;
 		 }
