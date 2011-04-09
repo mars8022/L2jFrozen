2eb741af01216866c4ade8686d4a82f12364d687
Shyla <Shyla@5215a6b0-ad85-4b3a-91a5-8dbf2feb3788>
2011-04-09 09:16:55 +0000

    Datapack Branch with last c3p0 support

---
+++ branches/c3p0_Rev/stable/datapack/sql/customs/Epic_MaskAndShield_SQL_OK.sql
@@ -0,0 +1,4 @@
+﻿delete from custom_weapon where item_id = 9802;
+delete from custom_armor where item_id = 9800;
+INSERT ignore INTO `custom_weapon` VALUES ('9802', 'Epic Shield', 'lhand', 'true', '1170', '0', '0', 's', '0', '0', 'none', '0', '0', '-8', '360', '50', '0', '0', '0', '-1', '50000', 1465, 'true', 'true', 'true', 'true', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
+INSERT ignore INTO `custom_armor` VALUES ('9800', 'Epic mask', 'face', 'false', 'none', 10,  'none', 0, -1, 0, 0, 0, 0, 0, 'true', 'true', 'true', 'true', 0, 0);
