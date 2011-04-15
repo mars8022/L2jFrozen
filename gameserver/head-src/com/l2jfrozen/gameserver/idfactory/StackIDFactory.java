0db724eb66539c5b817b43fb7275221d32ad5be6
Nefer <nefer@5215a6b0-ad85-4b3a-91a5-8dbf2feb3788>
2011-04-15 19:54:40 +0000

    - Fixed various bug, rework of IDFactory (Credits to Olympic new Initiate Dev) * Fixed random Teleport * Fixed random start items

--- gameserver/head-src/com/l2jfrozen/gameserver/idfactory/StackIDFactory.java
+++ gameserver/head-src/com/l2jfrozen/gameserver/idfactory/StackIDFactory.java
@@ -18,7 +18,6 @@
  */
 package com.l2jfrozen.gameserver.idfactory;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -26,12 +25,11 @@ import java.util.Stack;
 import java.util.logging.Logger;
 
 import com.l2jfrozen.Config;
-import com.l2jfrozen.util.CloseUtil;
 import com.l2jfrozen.util.database.L2DatabaseFactory;
 
 /**
  * This class ...
- * 
+ * @author Olympic
  * @version $Revision: 1.3.2.1.2.7 $ $Date: 2005/04/11 10:06:12 $
  */
 public class StackIDFactory extends IdFactory
@@ -45,144 +43,120 @@ public class StackIDFactory extends IdFactory
 
 	protected StackIDFactory()
 	{
-		super();
-		_curOID = FIRST_OID;
-		_tempOID = FIRST_OID;
+        super();
+        _curOID = FIRST_OID;
+        _tempOID = FIRST_OID;
 
-		Connection con = null;
+        java.sql.Connection con = null;
+        try
+        {
+            con = L2DatabaseFactory.getInstance().getConnection();
+            //con.createStatement().execute("drop table if exists tmp_obj_id");
 
-		try
-		{
-			con = L2DatabaseFactory.getInstance().getConnection(false);
-			//con.createStatement().execute("drop table if exists tmp_obj_id");
+            int[] tmp_obj_ids = extractUsedObjectIDTable();
+            if (tmp_obj_ids.length > 0)
+            {
+                _curOID = tmp_obj_ids[tmp_obj_ids.length-1];
+            }
+            System.out.println("Max Id = " + _curOID);
 
-			int[] tmp_obj_ids = extractUsedObjectIDTable();
+            int N = tmp_obj_ids.length;
+            for (int idx = 0; idx < N; idx++)
+            {
+                N = insertUntil(tmp_obj_ids, idx, N, con);
+            }
 
-			if(tmp_obj_ids.length > 0)
-			{
-				_curOID = tmp_obj_ids[tmp_obj_ids.length - 1];
-			}
-
-			System.out.println("Max Id = " + _curOID);
-
-			int N = tmp_obj_ids.length;
-
-			for(int idx = 0; idx < N; idx++)
-			{
-				N = insertUntil(tmp_obj_ids, idx, N, con);
-			}
-
-			_curOID++;
-			_log.config("IdFactory: Next usable Object ID is: " + _curOID);
-			_initialized = true;
-		}
-		catch(Exception e1)
-		{
+            _curOID++;
+            _log.config("IdFactory: Next usable Object ID is: " + _curOID);
+            _initialized = true;
+        }
+        catch (Exception e1)
+        {
 			e1.printStackTrace();
-			_log.severe("ID Factory could not be initialized correctly:" + e1);
-		}
-		finally
-		{
-			CloseUtil.close(con);
-			con = null;
-		}
+            _log.severe("ID Factory could not be initialized correctly:" + e1);
+        }
+        finally
+        {
+            try { con.close(); } catch (Exception e) {}
+        }
 	}
 
-	private int insertUntil(int[] tmp_obj_ids, int idx, int N, Connection con) throws SQLException
-	{
-		int id = tmp_obj_ids[idx];
+	private int insertUntil(int[] tmp_obj_ids, int idx, int N, java.sql.Connection con) throws SQLException
+    {
+        int id = tmp_obj_ids[idx];
+        if (id == _tempOID)
+        {
+        	_tempOID++;
+            return N;
+        }
+        // check these IDs not present in DB
+        if (Config.BAD_ID_CHECKING)
+        {
+        for (String check : ID_CHECKS)
+        {
+            PreparedStatement ps = con.prepareStatement(check);
+            ps.setInt(1, _tempOID);
+            //ps.setInt(1, _curOID);
+            ps.setInt(2, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next())
+            {
+                int badId = rs.getInt(1);
+                _log.severe("Bad ID " + badId + " in DB found by: " + check);
+                throw new RuntimeException();
+            }
+            rs.close();
+            ps.close();
+        }
+        }
 
-		if(id == _tempOID)
-		{
-			_tempOID++;
-			return N;
-		}
-
-		// check these IDs not present in DB
-		if(Config.BAD_ID_CHECKING)
-		{
-			for(String check : ID_CHECKS)
-			{
-				PreparedStatement ps = con.prepareStatement(check);
-				ps.setInt(1, _tempOID);
-				//ps.setInt(1, _curOID);
-				ps.setInt(2, id);
-				ResultSet rs = ps.executeQuery();
-
-				while(rs.next())
-				{
-					int badId = rs.getInt(1);
-					_log.severe("Bad ID " + badId + " in DB found by: " + check);
-					throw new RuntimeException();
-				}
-
-				rs.close();
-				ps.close();
-				rs = null;
-				ps = null;
-			}
-		}
-
-		//int hole = id - _curOID;
-		int hole = id - _tempOID;
-
-		if(hole > N - idx)
-		{
-			hole = N - idx;
-		}
-
-		for(int i = 1; i <= hole; i++)
-		{
-			//System.out.println("Free ID added " + (_tempOID));
-			_freeOIDStack.push(_tempOID);
-			_tempOID++;
-			//_curOID++;
-		}
-
-		if(hole < N - idx)
-		{
-			_tempOID++;
-		}
-
-		return N - hole;
-	}
+        //int hole = id - _curOID;
+        int hole = id - _tempOID;
+        if (hole > N - idx) hole = N - idx;
+        for (int i = 1; i <= hole; i++)
+        {
+            //System.out.println("Free ID added " + (_tempOID));
+            _freeOIDStack.push(_tempOID);
+            _tempOID++;
+            //_curOID++;
+        }
+        if (hole < N - idx) _tempOID++;
+        return N - hole;
+    }
 
 	public static IdFactory getInstance()
 	{
-		return _instance;
+        return _instance;
 	}
+
 
 	@Override
 	public synchronized int getNextId()
-	{
-		int id;
-
-		if(!_freeOIDStack.empty())
-		{
-			id = _freeOIDStack.pop();
-		}
-		else
-		{
-			id = _curOID;
-			_curOID = _curOID + 1;
-		}
-		return id;
-	}
+    {
+        	int id;
+        	if (!_freeOIDStack.empty())
+        		id = _freeOIDStack.pop();
+        	else
+        	{
+        		id = _curOID;
+        		_curOID = _curOID+1;
+        	}
+        	return id;
+    }
 
 	/**
 	 * return a used Object ID back to the pool
-	 * 
 	 * @param object ID
 	 */
 	@Override
 	public synchronized void releaseId(int id)
-	{
+    {
 		_freeOIDStack.push(id);
-	}
+    }
 
-	@Override
+    @Override
 	public int size()
-	{
-		return FREE_OBJECT_ID_SIZE - _curOID + FIRST_OID + _freeOIDStack.size();
-	}
+    {
+        return FREE_OBJECT_ID_SIZE - _curOID + FIRST_OID + _freeOIDStack.size();
+    }
 }
