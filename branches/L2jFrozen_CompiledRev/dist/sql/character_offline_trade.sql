aa18aa5e3c932d55df5d41206ed9c0844bce1c82
Shyla <Shyla@5215a6b0-ad85-4b3a-91a5-8dbf2feb3788>
2011-05-30 22:28:35 +0000

    [COMPILED_REV] New Compiled Rev with last updates

--- branches/L2jFrozen_CompiledRev/dist/sql/character_offline_trade.sql
+++ branches/L2jFrozen_CompiledRev/dist/sql/character_offline_trade.sql
@@ -1,4 +1,4 @@
-drop table `character_offline_trade`;
+drop table IF EXISTS `character_offline_trade`;
 CREATE TABLE `character_offline_trade` (
   `charId` int(11) NOT NULL,
   `time` bigint(20) unsigned NOT NULL DEFAULT '0',
@@ -7,7 +7,7 @@ CREATE TABLE `character_offline_trade` (
   PRIMARY KEY (`charId`)
 ) ;
 
-drop table `character_offline_trade_items`;
+drop table IF EXISTS `character_offline_trade_items`;
 CREATE TABLE `character_offline_trade_items` (
   `charId` int(10) NOT NULL DEFAULT '0',
   `item` int(10) NOT NULL DEFAULT '0',
