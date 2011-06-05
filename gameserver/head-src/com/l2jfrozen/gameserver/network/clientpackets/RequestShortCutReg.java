d18f2eb37f70a1258aa93f92c62cdf19f0a4cb5a
Nefer <nefer@5215a6b0-ad85-4b3a-91a5-8dbf2feb3788>
2011-06-05 08:53:55 +0000

    [CORE][CONFIG] Added FloodProtector on Macro system.

--- gameserver/head-src/com/l2jfrozen/gameserver/network/clientpackets/RequestShortCutReg.java
+++ gameserver/head-src/com/l2jfrozen/gameserver/network/clientpackets/RequestShortCutReg.java
@@ -56,6 +56,10 @@ public final class RequestShortCutReg extends L2GameClientPacket
 		L2PcInstance activeChar = getClient().getActiveChar();
 		if(activeChar == null)
 			return;
+		
+		//Macro exploit fix
+		if (!getClient().getFloodProtectors().getMacro().tryPerformAction("register macro"))
+			return;
 
 		switch(_type)
 		{
