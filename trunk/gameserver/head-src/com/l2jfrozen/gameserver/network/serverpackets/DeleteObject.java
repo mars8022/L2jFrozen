47c23afbd9c9a9ecbb267639f65c9919eabddbe2
Nefer <Nefer@5215a6b0-ad85-4b3a-91a5-8dbf2feb3788>
2012-12-30 12:24:52 +0000

    [CORE][SQL] - Fixed NPE with peacezone. - Added new check on attack request. - Cleanup packet RelationChanged.java. - Fixed issue with karma on summon display. - Cleanup packet StopMove.java. - Cleanup packet DeleteObject.java. - Corrected method sendPacket. - Added method SetPartyFind(int find) and GetPartyFind(). - Fixed bug with delete char. - Corrected Table heroes charId and not charid (Linux users could be problems).

--- trunk/gameserver/head-src/com/l2jfrozen/gameserver/network/serverpackets/DeleteObject.java
+++ trunk/gameserver/head-src/com/l2jfrozen/gameserver/network/serverpackets/DeleteObject.java
@@ -22,28 +22,28 @@ import com.l2jfrozen.gameserver.model.L2Object;
 
 /**
  * sample 0000: 1e 9b da 12 40 ....@ format d
- * 
  * @version $Revision: 1.3.2.1.2.3 $ $Date: 2005/03/27 15:29:39 $
  */
 public class DeleteObject extends L2GameServerPacket
 {
 	private static final String _S__1E_DELETEOBJECT = "[S] 12 DeleteObject";
 	private int _objectId;
-
+	
 	public DeleteObject(L2Object obj)
 	{
 		_objectId = obj.getObjectId();
 	}
-
+	
 	@Override
 	protected final void writeImpl()
 	{
 		writeC(0x12);
 		writeD(_objectId);
-		writeD(0x00); //c2
+		writeD(0x00); // c2
 	}
-
-	/* (non-Javadoc)
+	
+	/*
+	 * (non-Javadoc)
 	 * @see com.l2jfrozen.gameserver.serverpackets.ServerBasePacket#getType()
 	 */
 	@Override
@@ -51,5 +51,4 @@ public class DeleteObject extends L2GameServerPacket
 	{
 		return _S__1E_DELETEOBJECT;
 	}
-
 }
