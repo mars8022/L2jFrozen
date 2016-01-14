e7f77b63b89f5d0397090e9f6d0ec4a66bb1b791
Shyla <Shyla@5215a6b0-ad85-4b3a-91a5-8dbf2feb3788>
2016-01-14 14:57:14 +0000

    L2jFrozen Datapack trunk has been initialized to 1004 stable version

---
+++ datapack/trunk/sql/customs/Wings_SQL_OK.sql
@@ -0,0 +1,10 @@
+﻿delete from custom_armor where item_id in (9710,9711,9712,9713,9714,9715,9716);
+
+INSERT ignore INTO `custom_armor` VALUES 
+('9710', 'Angel Wings', 'hair', 'false', 'none', '10',  'none', '0', '-1', '0', '0', '0', '0', '0', 'true', 'true', 'true', 'true', '0', '0'),
+('9711', 'Fallen Angel Wings', 'hair', 'false', 'none', '10',  'none', '0', '-1', '0', '0', '0', '0', '0', 'true', 'true', 'true', 'true', '0', '0'),
+('9712', 'Gargoyle Wings', 'hair', 'false', 'none', '10',  'none', '0', '-1', '0', '0', '0', '0', '0', 'true', 'true', 'true', 'true', '0', '0'),
+('9713', 'Chaos Wings', 'hair', 'false', 'none', '10',  'none', '0', '-1', '0', '0', '0', '0', '0', 'true', 'true', 'true', 'true', '0', '0'),
+('9714', 'Eva Wings', 'hair', 'false', 'none', '10',  'none', '0', '-1', '0', '0', '0', '0', '0', 'true', 'true', 'true', 'true', '0', '0'),
+('9715', 'Netherworld Wings', 'hair', 'false', 'none', '10',  'none', '0', '-1', '0', '0', '0', '0', '0', 'true', 'true', 'true', 'true', '0', '0'),
+('9716', 'Succubus Wings', 'hair', 'false', 'none', '10', 'none', '0', '-1', '0', '0', '0', '0', '0', 'true', 'true', 'true', 'true', '0', '0');
---
+++ datapack/trunk/sql/updates/update_855.sql
@@ -0,0 +1,43 @@
+delete from droplist where itemid = 11954;
+INSERT IGNORE INTO `droplist` VALUES (21294,7884,1,1,1,127); -- Infernal Master
+INSERT IGNORE INTO `droplist` VALUES (21600,7884,1,1,1,15); -- Infernal Master 
+
+delete from droplist where itemid = 11974;
+INSERT IGNORE INTO `droplist` VALUES (22127,164,1,1,1,35); -- Common Item - Elysian 
+
+delete from droplist where itemid = 11970;
+INSERT IGNORE INTO `droplist` VALUES (22127,213,1,1,1,35); -- Common Item - Branch of The Mother Tree 
+
+delete from droplist where itemid = 11969;
+INSERT IGNORE INTO `droplist` VALUES (22127,7895,1,1,1,35); -- Common Item - Flaming Dragon Skull
+
+delete from droplist where itemid = 11966;
+INSERT IGNORE INTO `droplist` VALUES (22127,7902,1,1,1,35); -- Common Item - Doom Crusher
+
+delete from droplist where itemid = 12266;
+INSERT IGNORE INTO `droplist` VALUES (22146,5326,1,1,1,68); -- Common Item - Sealed Robe of Nightmare 
+
+delete from droplist where itemid = 12259;
+INSERT IGNORE INTO `droplist` VALUES (22146,5329,1,1,1,68); -- Common Item - Sealed Majestic Robe
+
+delete from droplist where itemid = 11968;
+INSERT IGNORE INTO `droplist` VALUES (22149,81,1,1,1,77); -- Common Item - Dragon Slayer
+INSERT IGNORE INTO `droplist` VALUES (22162,81,1,1,1,116); -- Common Item - Dragon Slayer
+
+delete from droplist where itemid = 11971;
+INSERT IGNORE INTO `droplist` VALUES (22149,151,1,1,1,77); -- Common Item - Sword of Miracles
+INSERT IGNORE INTO `droplist` VALUES (22162,151,1,1,1,116); -- Common Item - Sword of Miracles
+
+delete from droplist where itemid = 11973;
+INSERT IGNORE INTO `droplist` VALUES (22163,236,1,1,1,185); -- Common Item - Soul Separator
+
+delete from droplist where itemid = 12350;
+INSERT IGNORE INTO `droplist` VALUES (22154,6326,1,1,1,1183); -- Common Item - Sealed Majestic Necklace 
+
+delete from droplist where itemid = 12352;
+INSERT IGNORE INTO `droplist` VALUES (22154,6327,1,1,1,845); -- Common Item - Sealed Majestic Earring
+
+delete from droplist where itemid = 12351;
+INSERT IGNORE INTO `droplist` VALUES (22154,6328,1,1,1,1014); -- Common Item - Sealed Majestic Ring
+
+delete from droplist where itemid = 10587;
